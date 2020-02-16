37b0423eaf40d0d4a822bb8534e1146fcd41ff1d
SergiyAgeev <33694207+SergiyAgeev@users.noreply.github.com>
2020-02-16 23:15:30 +0200

    DB_properties (#2)

--- src/main/java/spring/intro/config/AppConfig.java
+++ src/main/java/spring/intro/config/AppConfig.java
@@ -7,22 +7,31 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import spring.intro.model.User;
 
 @Configuration
+@PropertySource("classpath:db.properties")
 @ComponentScan(basePackages = {
         "spring.intro.dao",
         "spring.intro.service"
 })
 public class AppConfig {
+    private final Environment env;
+
+    public AppConfig(Environment env) {
+        this.env = env;
+    }
+
     @Bean
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/spring_start?serverTimezone=EET");
-        dataSource.setUsername("root");
-        dataSource.setPassword("MyNewPass5!");
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
         return dataSource;
     }
 
@@ -31,14 +40,14 @@ public class AppConfig {
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
         factoryBean.setDataSource(getDataSource());
 
-        Properties properties = new Properties();
-        properties.put("hibernate.show_sql", "true");
-        properties.put("hibernate.hbm2ddl.auto", "create-drop");
-        properties.put("hibernate.format_sql", "true");
-        properties.put("hibernate.use_sql_comments", "false");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        Properties props = new Properties();
+        props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        props.put("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
+        props.put("hibernate.use_sql_comments", env.getProperty("hibernate.use_sql_comments"));
+        props.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
 
-        factoryBean.setHibernateProperties(properties);
+        factoryBean.setHibernateProperties(props);
         factoryBean.setAnnotatedClasses(User.class);
         return factoryBean;
     }
